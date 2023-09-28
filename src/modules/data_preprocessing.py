fff689647d744d1b37042bce5c8375ab0e549bac
Christian Leclerc <christianleclerc.info@gmail.com>
2023-09-28 19:19:31 -0400

    FIX: folder issue

---
+++ src/modules/data_preprocessing.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def clean_data(df):
+    # Cleaning steps here
+    return df_cleaned
+
+def feature_engineering(df):
+    # Feature engineering steps here
+    return df_engineered
