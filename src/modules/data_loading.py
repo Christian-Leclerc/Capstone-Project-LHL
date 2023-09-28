950be1df5cecfb39c2507fa98b6664b9f8674f6f
Christian Leclerc <christianleclerc.info@gmail.com>
2023-09-28 19:21:17 -0400

    FIX: folder issue 2

--- src/modules/data_loading.py
+++ src/modules/data_loading.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 def load_train_data():
-    return pd.read_csv('../../../data/train.csv')
+    return pd.read_csv('../../data/train.csv')
 
 def load_test_data():
-    return pd.read_csv('../../../data/test.csv')
+    return pd.read_csv('../../data/test.csv')
 
 def load_full_data():
-    return pd.read_csv('../../../data/full_data.csv')
+    return pd.read_csv('../../data/full_data.csv')
